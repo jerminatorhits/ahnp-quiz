67aa8429c578131aa31d942121055c995dee2731
Jeremy Kwa <jeremykwa@gmail.com>
2018-03-17 22:41:51 -0700

    replace sqlite with pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180318014501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "agent"
     t.integer "counter", default: 1
